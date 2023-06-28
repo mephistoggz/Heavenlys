e2abb07cca081f81a9e11238c39b78c492857eb1
mephistoggz <112123790+mephistoggz@users.noreply.github.com>
2023-06-28 03:29:44 +0300

    Initial commit

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+const client = global.client
+
+module.exports = () => {
+
+client.user.setActivity(`Heavenly Anime`)
+
+  
+}
+module.exports.config = {
+    name: "ready"
+}
